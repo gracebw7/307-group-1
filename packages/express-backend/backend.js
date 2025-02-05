a8ee8e97635725b1aea524cf57d2d01b43d7474b
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-02-05 09:16:39 -0800

    feat: initiated react, express, mongo, node

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,29 @@
+// backend.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users") // connect to Db "users"
+  .catch((error) => console.log(error));
+
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
