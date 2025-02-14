cc3c283e0672e743654fba69c9c463de7e4fffa2
mattacarregui <macarreg@calpoly.edu>
2025-02-13 18:17:05 -0800

    property form

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, Box } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Reviews from "./pages/Reviews";
+import AddProperty from "./pages/AddProperty";  // ✅ Import the new page
 
 function App() {
   return (
@@ -9,10 +10,12 @@ function App() {
         <Box p={4} bg="gray.100" minH="100vh">
           <Box as="nav" bg="white" p={4} mb={6} boxShadow="md">
             <Link to="/reviews" style={{ marginRight: "20px" }}>Reviews</Link>
+            <Link to="/add-property">Add Property</Link>  {/* ✅ New link */}
           </Box>
 
           <Routes>
             <Route path="/reviews" element={<Reviews />} />
+            <Route path="/add-property" element={<AddProperty />} />  {/* ✅ New route */}
           </Routes>
         </Box>
       </Router>
