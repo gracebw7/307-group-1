a7b23fd3968407407353374f24616380755924f8
mattacarregui <macarreg@calpoly.edu>
2025-02-24 14:12:20 -0800

    Revert "added tags to properties"
    This reverts commit 3b63c44aaf0dc49e430d3ee1d4750fc9f8a9c9f7.

--- packages/react-frontend/src/components/PropertyForm.jsx
+++ packages/react-frontend/src/components/PropertyForm.jsx
@@ -1,35 +1,23 @@
 import { useState } from "react";
-import { Box, Button, Input, FormLabel, VStack, NumberInput, NumberInputField } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Input,
+  FormLabel,
+  VStack
+} from "@chakra-ui/react";
 import PropTypes from "prop-types";
-import Select from "react-select";
 
 const PropertyForm = ({ onSubmit }) => {
   const [formData, setFormData] = useState({
     name: "",
-    address: "",
-    bedrooms: 0,
-    bathrooms: 0,
-    tags: [],
+    address: ""
   });
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleNumberChange = (valueAsString, valueAsNumber, name) => {
-    setFormData({
-      ...formData,
-      [name]: valueAsNumber,
-    });
-  };
-
-  const handleTagsChange = (selectedOptions) => {
-    setFormData({
-      ...formData,
-      tags: selectedOptions.map(option => option.value),
+      [e.target.name]: e.target.value
     });
   };
 
@@ -37,12 +25,6 @@ const PropertyForm = ({ onSubmit }) => {
     e.preventDefault();
     onSubmit(formData);
   };
-
-  const tagOptions = [
-    { value: "apartment", label: "Apartment" },
-    { value: "house", label: "House" },
-    { value: "close-to-campus", label: "Close to Campus" },
-  ];
 
   return (
     <Box p={4} bg="white" borderRadius="lg" boxShadow="md">
@@ -70,44 +52,6 @@ const PropertyForm = ({ onSubmit }) => {
             />
           </Box>
 
-          <Box>
-            <FormLabel>Bedrooms</FormLabel>
-            <NumberInput
-              min={0}
-              name="bedrooms"
-              value={formData.bedrooms}
-              onChange={(valueAsString, valueAsNumber) => handleNumberChange(valueAsString, valueAsNumber, "bedrooms")}
-              required
-            >
-              <NumberInputField />
-            </NumberInput>
-          </Box>
-
-          <Box>
-            <FormLabel>Bathrooms</FormLabel>
-            <NumberInput
-              min={0}
-              name="bathrooms"
-              value={formData.bathrooms}
-              onChange={(valueAsString, valueAsNumber) => handleNumberChange(valueAsString, valueAsNumber, "bathrooms")}
-              required
-            >
-              <NumberInputField />
-            </NumberInput>
-          </Box>
-
-          <Box>
-            <FormLabel>Tags</FormLabel>
-            <Select
-              isMulti
-              name="tags"
-              options={tagOptions}
-              className="basic-multi-select"
-              classNamePrefix="select"
-              onChange={handleTagsChange}
-            />
-          </Box>
-
           <Button type="submit" colorScheme="blue">
             Add Property
           </Button>
@@ -118,7 +62,7 @@ const PropertyForm = ({ onSubmit }) => {
 };
 
 PropertyForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
 };
 
 export default PropertyForm;
