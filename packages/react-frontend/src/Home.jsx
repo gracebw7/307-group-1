2f10ec150e7b1db03f4387060ce8fe871fc7c98c
Kyle Lin <linkyle0924@gmail.com>
2025-03-03 00:42:51 -0800

    Add Search Bar

--- packages/react-frontend/src/Home.jsx
+++ packages/react-frontend/src/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text, Box } from "@chakra-ui/react";
+import { Text, Box, Input } from "@chakra-ui/react";
 
 function Home() {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" height="80vh">
+    <Box display="flex" justifyContent="center" alignItems="center" height="80vh" flexDirection="column">
       <Text fontSize="100" fontWeight="bold">PropHunt</Text>
+      <Input
+        placeholder="Search..."
+        width="300px"
+        mt={4}
+      />
     </Box>
   );
 }
