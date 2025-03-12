8ba6f9251767b0c7e4bb1c3be1a6fe57871f3a15
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-03-12 13:00:19 -0700

    feat: add logout message

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -33,11 +33,13 @@ import { ReviewsProvider } from "./reviewsContext";
 import PropertyPage from "./pages/PropertyPage";
 import Home from "./pages/Home";
 import PHLogo from "./assets/PHLogo.png";
+import { useNavigate } from "react-router-dom";
 
 const API_PREFIX = "https://prophuntapi.azurewebsites.net";
 
 function App() {
   const INVALID_TOKEN = "INVALID_TOKEN";
+  const navigate = useNavigate();
   const [token, setToken] = useState(
     localStorage.getItem("token") || INVALID_TOKEN
   );
@@ -96,8 +98,7 @@ function App() {
   const logoutUser = () => {
     localStorage.removeItem("token");
     setToken(INVALID_TOKEN);
-    setMessage("Logged out");
-    //window.location.href = "/";
+    navigate("/");
   };
 
   const linkStyle = {
