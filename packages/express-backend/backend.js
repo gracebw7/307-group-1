bc4a9d532a5634cd9635ff8544d767601adc6409
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-03-12 09:14:34 -0700

    Revert "changed port"
    This reverts commit 9696f0130071d7aa6187d1433b1256a350631281.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -22,7 +22,7 @@ mongoose
   .catch((error) => console.log(error));
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -259,5 +259,5 @@ app.delete("/properties/_id", (req, res) => {
 */
 
 app.listen(process.env.PORT || port, () => {
-  console.log("Rest API is listening.");
+  console.log("REST API is listening.");
 });
